63339cf80ae1e4451216922f15bce618057cdb43
hanchen <hanchenjhc@gmail.com>
2020-06-13 16:44:57 -0500

    change heap to use index implementation instead

--- Heap.go
+++ Heap.go
@@ -4,16 +4,16 @@ type Heap struct {
 	data []int
 }
 
-func (heap Heap) parent(i int) *int {
-	return &heap.data[(i-1)/2]
+func (heap Heap) parent(i int) int {
+	return (i - 1) / 2
 }
 
-func (heap Heap) left(i int) *int {
-	return &heap.data[i*2+1]
+func (heap Heap) left(i int) int {
+	return i*2 + 1
 }
 
-func (heap Heap) right(i int) *int {
-	return &heap.data[(i+1)*2]
+func (heap Heap) right(i int) int {
+	return (i + 1) * 2
 }
 
 func (heap Heap) hasLeft(i int) bool {
@@ -24,36 +24,21 @@ func (heap Heap) hasRight(i int) bool {
 	return len(heap.data) > (i+1)*2
 }
 
-func (heap Heap) maxHeapify(index int) Heap {
-	current := heap.data[index]
-	if heap.hasLeft(index) {
-		left := *heap.left(index)
+func (heap Heap) maxHeapify(index int) {
+	indexOfLargest := index
 
-		if heap.hasRight(index) {
-			right := *heap.right(index)
-
-			if left > current && left > right {
-				heap.data[index] = left
-				*heap.left(index) = current
-
-				return heap.maxHeapify(index*2 + 1)
-			}
-
-			if right > current && right > left {
-				heap.data[index] = right
-				*heap.right(index) = current
-
-				return heap.maxHeapify((index + 1) * 2)
-			}
-		} else {
-			if left > current {
-				heap.data[index] = left
-				*heap.left(index) = current
-
-				return heap.maxHeapify(index*2 + 1)
-			}
-		}
+	if heap.hasLeft(index) && heap.data[heap.left(index)] > heap.data[indexOfLargest] {
+		indexOfLargest = heap.left(index)
 	}
 
-	return heap
+	if heap.hasRight(index) && heap.data[heap.right(index)] > heap.data[indexOfLargest] {
+		indexOfLargest = heap.right(index)
+	}
+
+	if indexOfLargest != index {
+		temp := heap.data[index]
+		heap.data[index] = heap.data[indexOfLargest]
+		heap.data[indexOfLargest] = temp
+		heap.maxHeapify(indexOfLargest)
+	}
 }
